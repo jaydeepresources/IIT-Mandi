42b85ae9c7b0f74102f90119b19c2003ad4fe4be
jaydeep apte <jaydeepresources@gmail.com>
2023-09-21 20:04:41 +0530

    Add files via upload

---
+++ Conditions/if.cpp
@@ -0,0 +1,18 @@
+// < > <= >= == !=
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+
+    if (a > 0)
+        cout << "+ve";
+    else if (a < 0)
+        cout << "-ve";
+    else
+        cout << "0";
+
+    return 0;
+}
